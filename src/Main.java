24b1ba1e79c99190037c9cd18f49fc84dc8d53ae
Bliri <bliri.berisha@student.uni-pr.edu>
2026-01-21 12:58:01 +0100

    fix gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("🐉 Welcome to Dragon Ball Battle Game!");
+        System.out.println("🐉 Welcome to Dragon Ball Battle Game!!");
 
    
         
