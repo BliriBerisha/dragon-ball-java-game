bbba6b0b06aabd8ac738c345982b87dd1ec5d260
Bliri <bliri.berisha@student.uni-pr.edu>
2026-01-28 00:20:22 +0100

    added vegeta forms + MSSJB

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 
-import characters.Beerus;
-import tools.FormStatsTable;
+import characters.Goku;
+import characters.Vegeta;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Dragon Ball Battle Game!!");
 
-        Beerus beerus = new Beerus();
-        FormStatsTable.printFor(beerus);
+        Goku goku = new Goku();
+        goku.transformTo("SuperSaiyan","BLUE" );
+                goku.showStats();
+System.out.println(" x : x");
+        goku.transformTo("KAIOKEN", "X20");
+        goku.showStats();
+
+System.out.println(" x : x");
+
+        Vegeta vegeta = new Vegeta();
+        vegeta.transformTo("SuperSaiyan", "MASTERED BLUE");
+        vegeta.showStats();
        
     }
 }
--- src/characters/Vegeta.java
+++ src/characters/Vegeta.java
@@ -1,12 +1,21 @@
 package characters;
 
+import transformations.SuperSaiyan;
+
 public class Vegeta extends Fighter{
     
     public Vegeta() {
-        super("Vegeta", 100,20,100,9000,10);
+        super("Vegeta", 100,20,100,1000,10);
 
         //register forms
-
+  addTransformations(new SuperSaiyan("1", 1.1, 1.15 ,1.20 ,1.5 ,1.4)); // SSJ1
+        addTransformations(new SuperSaiyan("2", 1.15, 1.25, 1.35, 2, 1.8)); // SSJ2
+        addTransformations(new SuperSaiyan("3", 1.25, 1.35, 1.6, 2.7, 2.2 )); // SSJ3
+        addTransformations(new SuperSaiyan("4", 1.3, 1.4 , 2, 3.2, 2.6)); // SSJ4
+        // Super SSGSS's
+        addTransformations(new SuperSaiyan("GOD", 1.45, 1.7, 2.7, 3.6, 3.0)); // SSJ GOD
+        addTransformations(new SuperSaiyan("BLUE", 1.5, 1.75, 3.2, 4.2, 3.8));
+        addTransformations(new SuperSaiyan("MASTERED BLUE", 1.6, 1.8, 5.0, 5.0, 5.0)); // SSJ MASTERED BLUE
     }
     @Override
     public void specialMoveUse(Fighter fighter) {
